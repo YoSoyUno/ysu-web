0c9319fa80ef2c6c2dcda001be0e084123c0c66c
Yaco <franco@reevo.org>
2017-01-07 14:43:11 -0300

    agrega traduccion al español

---
+++ www/red/mod/metatags/languages/es.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package Metatags
+ */
+
+$es = array(
+        //Mainpage settings
+        'metatags:mainpage:title' => 'Escribe un título para la página principal',
+        'metatags:mainpage:keywords' => 'Escribe palabras clave que describen tu sitio. Serán utilizadas para la portada',
+        'metatags:mainpage:description' => 'Escribe una descripción para tu sitio. Un consejo, utiliza palabras que hayas incluído en la página principal para mejores resultados',
+        'metatags:mainpage:image' => 'Incluye un archivo de imagen que represente tu sitio. Este archivo deberá estar ubidado en la carpeta /_graphics',
+        'metatags:cloudflare' => 'Quieres que las URLs de los avatar sean estáticas?',
+
+        //setttings voor plugins
+        'blog:message:description' => 'publicó una noticia',
+        'blog:title:preformat' => 'Blog - %s',
+        'market:message:description' => 'quiere compartir esta oferta',
+        'market:title:preformat' => '%s - publicidad',
+);
