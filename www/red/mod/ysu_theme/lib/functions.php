35820f265e3e6e16c14b0c2c7e9e4d2e6cc672ac
Yaco <franco@reevo.org>
2017-01-10 00:13:33 -0300

    intenta corregir funcion de is_viajero

--- www/red/mod/ysu_theme/lib/functions.php
+++ www/red/mod/ysu_theme/lib/functions.php
@@ -4,10 +4,12 @@ function is_viajero($user) {
 
   $viajeros = explode(",", elgg_get_plugin_setting('viajeros', 'ysu_theme'));
   foreach ($viajeros as $viajero) {
-    $guid = get_user_by_username($viajero);
+    if (get_user_by_username($viajero)) {
+      $guid = get_user_by_username($viajero);
 
-    if ($guid->getGUID() == $user) {
-      return true;
+      if ($guid->getGUID() == $user) {
+        return true;
+      }
     }
   return false;
   }
