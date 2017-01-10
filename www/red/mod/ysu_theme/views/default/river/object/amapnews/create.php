7d748104ee9fab182a0710537e0cc84cb7bc5f66
Yaco <franco@reevo.org>
2017-01-10 09:47:48 -0300

    elimina clase innecesaria

--- www/red/mod/ysu_theme/views/default/river/object/amapnews/create.php
+++ www/red/mod/ysu_theme/views/default/river/object/amapnews/create.php
@@ -10,6 +10,5 @@ $excerpt = elgg_get_excerpt($object->excerpt);
 echo elgg_view('river/elements/layout-news', array(
     'item' => $vars['item'],
     'message' => $excerpt,
-    'item_class' => 'xxx'
     //'attachments' => elgg_view('output/url', array('href' => $object->address)),
 ));
