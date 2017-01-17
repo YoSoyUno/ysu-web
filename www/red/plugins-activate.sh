54afe64ab549829498b3592619a10d08303ccb47
Yaco <franco@reevo.org>
2017-01-17 11:03:13 -0300

    incluye script para activar plugins por defecto

---
+++ www/red/plugins-activate.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PLUGINS=`awk -vORS=, '{ print $1 }' plugins.list | sed 's/,$/\n/' | sed 's/,,/,/g' | sed 's/,*$//g'`
+
+# desactiva todos los plugins
+vendor/bin/elgg-cli plugins:deactivate --all
+
+# activa todos los listados en plugins.list en el orden sugerido
+vendor/bin/elgg-cli plugins:activate "$PLUGINS"
