265a013d1bf8cb35cbc4b0b8a719232ae16f7a39
Yaco <franco@reevo.org>
2017-01-17 13:04:48 -0300

    mejora lectura de output

--- www/red/plugins-activate.sh
+++ www/red/plugins-activate.sh
@@ -5,5 +5,9 @@ PLUGINS=`awk -vORS=, '{ print $1 }' plugins.list | sed 's/,$/\n/' | sed 's/,,/,/
 # desactiva todos los plugins
 vendor/bin/elgg-cli plugins:deactivate --all
 
+echo ""
+echo "------------------"
+echo ""
+
 # activa todos los listados en plugins.list en el orden sugerido
 vendor/bin/elgg-cli plugins:activate "$PLUGINS"
