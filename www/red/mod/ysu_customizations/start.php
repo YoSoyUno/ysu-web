1d218e329ddce0c618d46027f0668f0ee1d9044b
Yaco <franco@reevo.org>
2017-01-20 16:55:41 -0300

    al crear un grupo, las herramientas se activan en funcin del tipo de punto

--- www/red/mod/ysu_customizations/start.php
+++ www/red/mod/ysu_customizations/start.php
@@ -7,9 +7,41 @@ require_once(dirname(__FILE__) . '/lib/functions.php');
  *
  * @return void
  */
+ // register default elgg events
+elgg_register_event_handler('init', 'system', 'ysu_customizations_init');
+
+ // Register a handler for create groups
+elgg_register_event_handler('create', 'group', 'groups_create_event_tools');
+
 function ysu_customizations_init() {
 	elgg_extend_view('css/elgg', 'css/ysu_customizations.css');
 }
 
-// register default elgg events
-elgg_register_event_handler('init', 'system', 'ysu_customizations_init');
+function groups_create_event_tools($event, $object_type, $object) {
+
+	$tipo_punto = $object->tipo;
+
+	switch ($tipo_punto) {
+		case 'poder':
+			$object->activity_enable = 'no';
+			$object->tp_images_enable = 'no';
+			$object->membersmap_enable = 'no';
+			$object->related_groups_enable = 'no';
+			$object->amapnews_enable = 'yes';
+			$object->photos_enable = 'yes';
+			$object->event_manager_enable = 'yes';
+			break;
+		case 'paso':
+			$object->activity_enable = 'no';
+			$object->tp_images_enable = 'no';
+			$object->membersmap_enable = 'no';
+			$object->related_groups_enable = 'no';
+			$object->amapnews_enable = 'yes';
+			$object->photos_enable = 'yes';
+			$object->event_manager_enable = 'no';
+			break;
+		default:
+			# code...
+			break;
+	}
+}
