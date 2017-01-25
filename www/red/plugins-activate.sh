16bbe3a8cd50cd439ca020b1d8d540966890f765
Yaco <franco@reevo.org>
2017-01-25 17:13:51 -0300

    activa plugin de developers si estamos en alfa o beta

--- www/red/plugins-activate.sh
+++ www/red/plugins-activate.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source ../../bin/database_auth.sh
+
 PLUGINS=`awk -vORS=, '{ print $1 }' plugins.list | sed 's/,$/\n/' | sed 's/,,/,/g' | sed 's/,*$//g'`
 
 # desactiva todos los plugins
@@ -10,4 +12,15 @@ echo "------------------"
 echo ""
 
 # activa todos los listados en plugins.list en el orden sugerido
-vendor/bin/elgg-cli plugins:activate "$PLUGINS"
+# y activa plugins de desarrollo en funcion si estamos en entornos de desarrollo
+if [ $VERSION == "ALFA" ] || [ $VERSION == "BETA" ]
+then
+  vendor/bin/elgg-cli plugins:activate "$PLUGINS,developers"
+else
+  vendor/bin/elgg-cli plugins:activate "$PLUGINS"
+fi
+
+#limpia caches
+echo ""
+echo "Limpio el cache..."
+vendor/bin/elgg-cli site:flush_cache
