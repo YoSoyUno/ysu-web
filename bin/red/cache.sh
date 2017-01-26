496b73c0b1065eab88248018235dddc3ebf19515
Yaco <franco@reevo.org>
2017-01-26 20:37:58 -0300

    usa rutas absolutas para correrlo desde cualquier punto

--- bin/red/cache.sh
+++ bin/red/cache.sh
@@ -5,11 +5,11 @@
 
 if [ $1 == "0" ]
 then
-  ../../www/red/vendor/bin/elgg-cli config:setting system_cache_enabled 0
-  ../../www/red/vendor/bin/elgg-cli config:setting simplecache_enabled 0
+  /srv/ysu-web/www/red/vendor/bin/elgg-cli config:setting system_cache_enabled 0
+  /srv/ysu-web/www/red/vendor/bin/elgg-cli config:setting simplecache_enabled 0
   echo "Cache desactivados!"
 else
-  ../../www/red/vendor/bin/elgg-cli config:setting system_cache_enabled 1
-  ../../www/red/vendor/bin/elgg-cli config:setting simplecache_enabled 1
+  /srv/ysu-web/www/red/vendor/bin/elgg-cli config:setting system_cache_enabled 1
+  /srv/ysu-web/www/red/vendor/bin/elgg-cli config:setting simplecache_enabled 1
   echo "Cache activados!"
 fi
